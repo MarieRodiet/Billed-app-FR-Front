3ff31ac38edddbe5f61df69485cbdb7ac23abf0f
Marie Rodiet <marie.rodiet@gmail.com>
2022-03-30 16:32:27 +0200

    Add E2E Employee plan, Comment code

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -3,9 +3,8 @@
  */
 
 import { fireEvent, screen, waitFor } from "@testing-library/dom"
-import { userEvent } from "@testing-library/user-event"
 import '@testing-library/jest-dom/extend-expect'
-import mockStore from "../__mocks__/store"
+import mockStore from "../__mocks__/store.js"
 import { localStorageMock } from "../__mocks__/localStorage.js";
 import { bills } from "../fixtures/bills.js"
 import { ROUTES_PATH, ROUTES } from "../constants/routes.js";
@@ -268,6 +267,40 @@ describe("Given I am connected as an employee", () => {
 
       expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
       expect(screen.getByTestId("btn-new-bill")).toBeTruthy();
+    })
+
+    //add async in the arrow function in test
+    test("it should sucessfully create a new bill with a POST request", async () => {
+      Object.defineProperty(window, 'localStorage', { value: localStorageMock })
+      window.localStorage.setItem('user', JSON.stringify({
+        type: 'Employee'
+      }))
+      const root = document.createElement("div");
+      root.setAttribute("id", "root");
+      document.body.append(root);
+
+      const postSpy = jest.spyOn(mockStore, "bills");
+
+      const newBillData = {
+        "id": "UIUZtnPQvnbFnB0ozvJh",
+        "name": "test3",
+        "email": "a@a",
+        "type": "Services en ligne",
+        "vat": "60",
+        "pct": 20,
+        "commentAdmin": "bon bah d'accord",
+        "amount": 300,
+        "status": "accepted",
+        "date": "2003-03-03",
+        "commentary": "",
+        "fileName": "facture-client-php-exportee-dans-document-pdf-enregistre-sur-disque-dur.png",
+        "fileUrl": "https://test.storage.tld/v0/b/billable-677b6.a…dur.png?alt=media&token=571d34cb-9c8f-430a-af52-66221cae1da3"
+      };
+
+      const bills = await mockStore.create(newBillData)
+      expect(postSpy).toHaveBeenCalledTimes(1);
+      expect(bills).toBe("test3")
+
     })
 
   })
