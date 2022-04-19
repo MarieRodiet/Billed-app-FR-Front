e12666309b0bf6fac8a067de6b4b962a74d7ef3b
Marie Rodiet <marie.rodiet@gmail.com>
2022-04-19 15:09:35 +0200

    Respect naming convention for test files

--- src/views/LoginUI.js
+++ src/views/LoginUI.js
@@ -1,5 +1,5 @@
 export default () => {
-    return `
+  return (`
       <div class="page-div">
         <div class="row">
           <div class="title-container"> 
@@ -17,9 +17,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-employee">
                   <h2 class="h3 mb-3 font-weight-normal">Employé</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autocomplete="on" autofocus>
+                  <input type="email" data-testid="employee-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
-                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" autocomplete="on" required>
+                  <input type="password" data-testid="employee-password-input" class="form-control" placeholder="******" required>
                   <button class="btn btn-lg btn-primary btn-block" data-testid="employee-login-button" style="background-color: #0E5AE5;" type="submit">Se connecter</button>
                 </form>
               </div>
@@ -31,9 +31,9 @@ export default () => {
                 <form class="form-signin" data-testid="form-admin">
                   <h2 class="h3 mb-3 font-weight-normal">Administration</h1>
                   <label for="inputEmail">Votre email</label>
-                  <input autocomplete="on" type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
+                  <input type="email" data-testid="admin-email-input" class="form-control" placeholder="johndoe@email.com" required autofocus>
                   <label for="inputPassword">Mot de passe</label>
-                  <input autocomplete="on" type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
+                  <input type="password" data-testid="admin-password-input" class="form-control" placeholder="******" required>
                   <button  type="submit" class="btn btn-lg btn-primary btn-block" data-testid="admin-login-button" style="background-color: #0E5AE5;">Se connecter</button>
                 </form>
               </div>
@@ -41,5 +41,5 @@ export default () => {
           </div>
         </div>
       </div>
-  `;
+  `)
 };
